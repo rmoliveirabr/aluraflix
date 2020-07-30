b316d4d10ef46d0d88299133d4c100fa5c1cd401
Rodrigo <rmoliveirabr@gmail.com>
2020-07-30 18:19:19 -0300

    Changes on App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,18 @@ function App() {
         <Carousel
           category = {dadosIniciais.categorias[2]}
         />
+
+        <Carousel
+          category = {dadosIniciais.categorias[3]}
+        />
+
+        <Carousel
+          category = {dadosIniciais.categorias[4]}
+        />
+
+        <Carousel
+          category = {dadosIniciais.categorias[5]}
+        />
     </div>
   );
 }
